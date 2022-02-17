3d49aa5f747ba6bb66f7ea8f91001d5371501666
im ranjitha <ranjithatk101@gmail.com>
2022-02-17 22:34:03 +0530

    added changes

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express= require("express")   
 const app=express()
+const PORT=process.env.PORT || 3000
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
 app.get("/ranji",(req,res)=>{
     res.send("hello ranjimol")
 })
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("listening to port 3000")
 })
