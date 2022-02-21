85fe406a9af4c36a0a9f5aba820126ad4ca68676
im ranjitha <ranjithatk101@gmail.com>
2022-02-21 23:49:40 +0530

    added

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const express= require("express")   
+const express= require("express")  
+ const mongoose = require("mongoose")
+ mongoose.connect("mongodb+srv://RANJITHATK:RANJITHATK@cluster0.irwxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+    // useNewUrlParser:true,
+    // useUnifiedTopology:true
+}).then(()=>{
+    console.log("database connected")
+}).catch((err)=>{console.log(err)})
 const app=express()
 const PORT=process.env.PORT || 3000
 app.get("/",(req,res)=>{
