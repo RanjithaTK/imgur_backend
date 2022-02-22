d397c39fb46d8fe49bc2aa15a13e8aa46f7de98d
im ranjitha <ranjithatk101@gmail.com>
2022-02-22 10:31:54 +0530

    changes

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const express= require("express")  
- const mongoose = require("mongoose")
- mongoose.connect("mongodb+srv://RANJITHATK:RANJITHATK@cluster0.irwxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+const express=require("express")
+const mongoose = require("mongoose")
+const app=express()
+require("dotenv").config();     //to read environment variables from .env file
+app.use(express.json())  // parse requests of content-type - application/json
+app.use(express.urlencoded({       // parse requests of content-type - application/x-www-form-urlencoded
+    extended: true
+  }));
+
+// const user= require("./router/userRouter")
+// user(app)
+require("./router/userRouter")(app)            //while importing we have to give some argument bcz we r importing some function
+require("./router/imageRouter")(app)
+// require("./router/computerRouter")(app)
+
+const PORT=process.env.PORT || 3000    //setup server to listen on port 8080 or 3000
+mongoose.connect("mongodb+srv://RANJITHATK:RANJITHATK@cluster0.irwxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     // useNewUrlParser:true,
     // useUnifiedTopology:true
 }).then(()=>{
     console.log("database connected")
 }).catch((err)=>{console.log(err)})
-const app=express()
-const PORT=process.env.PORT || 3000
 app.get("/",(req,res)=>{
-    res.send("hello world")
-})
-app.get("/ranji",(req,res)=>{
-    res.send("hello ranjimol")
+    res.send("hi everyone")
 })
 app.listen(PORT,()=>{
-    console.log("listening to port 3000")
+    console.log(`listening to port ${PORT}`)
 })
